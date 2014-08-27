d7f2132c89974fc11770b4ff5d11edeffd414e61
jojofabe <jojofabe@vt.edu>
2014-08-26 20:52:45 -0400

    modified the source code for Problem 2

--- Problem2/Problem2.scala
+++ Problem2/Problem2.scala
@@ -6,27 +6,18 @@
 //values do not exceed four million, find the sum of the even-valued terms.
 
 object Problem2 {
-	var avg = 0;  
-	var list = List(); 
+
 	def main(args: Array[String]){
-		//avg = 0; 
-		var sum, x = 0
-		sum = 0
-		for(a <- 1 to 40000000){
-			x = fibonacci(a)
-			if(x % 2 == 0)
-		  		sum += fibonacci(a);
-		 }
-		 println(sum)
+		var a, b, sumEven = 0
+		for (i <- 0 to 4000000){
+			i = a + b
+			b = i
+			a = b
+			if(b % 2 == 0){
+				sumEven += sumEven + i
+			}
+		}
+		println(sumEven)
 	}
 	  
-	//still need to improve this algorithm's run time
-	def fibonacci(i: Integer): Integer={
-		//print(i + ", ");	
-		if(i == 1 || i == 0 || i == 2) return i; 
-		else{ 
-			return fibonacci(i - 1) + fibonacci(i - 2); 
-		}
-
-	}
 }
